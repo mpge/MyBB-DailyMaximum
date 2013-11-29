10b4563a9ba5b946d9929e1b6d1a94d40701b692
Matthew Gross <MatthewGross@users.noreply.github.com>
2013-11-29 07:06:49 -0500

    Create dailymaximum.php

---
+++ dailymaximum.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * DailyMaximum
+ * Copyright 2013 Ziloi (PluginBIN) (License: MIT)
+ * Developed for Ziloi (PluginBIN) by Matthew Gross (http://mattgross.net)
+ * http://www.pluginbin.com/
+ */
+ 
+ if (!defined("IN_MYBB"))
+ {
+  die("You CANNOT Access this File Directly!");
+ }
+ 
+ function dailymaximum_info()
+ {
+  "name" => 
+ }
+?>
